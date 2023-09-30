2ec8558d31032ec751c58c763c384655bd936070
mxtdev <maxta.gamer@gmail.com>
2023-09-30 02:01:12 -0300

    update

--- JavaScript/test/test.js
+++ JavaScript/test/test.js
@@ -1,1 +1,16 @@
-const test = "test";
+const nome = "Elleylson";
+const sobrenome = "santtos";
+const idade = 24;
+const altura = 1.79;
+const peso = 75;
+
+let imc = peso / altura ** 2;
+let anoNascimento = 2023 - idade;
+
+console.log(
+  `
+    ${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kilos tem ${altura} de altura, e seu IMC é de 
+    ${imc.toFixed(2)},
+    ${nome} ${sobrenome} nasceu em ${anoNascimento}
+  `
+);
