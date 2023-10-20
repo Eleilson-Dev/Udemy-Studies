a6869468c4cb08c1ec676f4b3e27e6de521ee825
Eleilson-Dev <maxta.gamer@gmail.com>
2023-10-20 19:08:59 -0300

    update

---
+++ JavaScript/002-Avaliacao-de-Curto-Circuito-(Short-Circuit)/js/script.js
@@ -0,0 +1,7 @@
+// com o operador logico && => AND
+const verdadeiro = true;
+console.log(verdadeiro && "o codigo chegou aqui");
+
+// com o operador logico || => OR
+const corEscolhida = "Vermelho";
+console.log(corEscolhida || "Preto");
