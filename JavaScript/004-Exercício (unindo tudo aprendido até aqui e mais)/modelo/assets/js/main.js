7eba320ef7cf68568bc46c45955fdb7b6750def2
Eleilson-Dev <maxta.gamer@gmail.com>
2023-10-25 20:40:35 -0300

    update

--- JavaScript/004-Exercício (unindo tudo aprendido até aqui e mais)/modelo/assets/js/main.js
+++ JavaScript/004-Exercício (unindo tudo aprendido até aqui e mais)/modelo/assets/js/main.js
@@ -1,44 +1,46 @@
 const btn = document.querySelector("#btn");
 const result = document.querySelector(".result");
+const peso = document.querySelector(".peso");
+const altura = document.querySelector(".altura");
 const p = result.querySelector("p");
 
 const imcs = [
-  { value: 18.5, status: "Abaixo do peso" },
-  { value: 24.9, status: "Peso normal" },
-  { value: 25, status: "Sobrepeso" },
-  { value: 29.9, status: "Sobrepeso" },
-  { value: 30, status: "Obesidade grau 1" },
-  { value: 34.9, status: "Obesidade grau 1" },
-  { value: 35, status: "Obesidade grau 2" },
-  { value: 39.9, status: "Obesidade grau 2" },
-  { value: 40, status: "Obesidade grau 3" },
+  { value: 18.5, status: "Abaixo do peso", color: "rgb(230, 19, 82)" },
+  { value: 24.9, status: "Peso normal", color: "rgb(64, 241, 132)" },
+  { value: 25, status: "Sobrepeso", color: "rgb(230, 146, 19)" },
+  { value: 29.9, status: "Sobrepeso", color: "rgb(230, 146, 19)" },
+  { value: 30, status: "Obesidade grau 1", color: "rgb(230, 19, 82)" },
+  { value: 34.9, status: "Obesidade grau 1", color: "rgb(230, 19, 82)" },
+  { value: 35, status: "Obesidade grau 2", color: "rgb(230, 19, 82)" },
+  { value: 39.9, status: "Obesidade grau 2", color: "rgb(230, 19, 82)" },
+  { value: 40, status: "Obesidade grau 3", color: "rgb(230, 19, 82)" },
 ];
 
-const calcIMC = (myIMC) => {
-  if (myIMC < imcs[0].value) {
-    p.innerHTML = `Seu IMC é ${myIMC}(${imcs[0].status})`;
-    p.style.background = "red";
-  } else if (myIMC >= imcs[0].value && myIMC <= imcs[1].value) {
-    p.innerHTML = `Seu IMC é ${myIMC}(${imcs[1].status})`;
-    p.style.background = "rgb(64, 241, 132)";
-  } else if (myIMC >= imcs[2].value && myIMC <= imcs[3].value) {
-    p.innerHTML = `Seu IMC é ${myIMC}(${imcs[2].status})`;
-    p.style.background = "rgb(247, 191, 34)";
-  } else if (myIMC >= imcs[4].value && myIMC <= imcs[5].value) {
-    p.innerHTML = `Seu IMC é ${myIMC}(${imcs[4].status})`;
-    p.style.background = "red";
-  } else if (myIMC >= imcs[6].value && myIMC <= imcs[7].value) {
-    p.innerHTML = `Seu IMC é ${myIMC}(${imcs[6].status})`;
-    p.style.background = "red";
-  } else if (myIMC >= 40) {
-    p.innerHTML = `Seu IMC é ${myIMC}(${imcs[8].status})`;
-    p.style.background = "red";
+const setColorAndText = (myIMC, status, color) => {
+  p.innerHTML = `Seu IMC é ${myIMC}(${status})`;
+  p.style.background = color;
+};
+
+const getIMCStatus = (myIMC) => {
+  for (const imc of imcs) {
+    if (myIMC < imc.value) {
+      return imc;
+    }
   }
+  return imcs[imcs.length - 1]; // Default to the last status if no match is found
+};
+
+const calcIMC = (myIMC) => {
+  const imcStatus = getIMCStatus(myIMC);
+  setColorAndText(myIMC, imcStatus.status, imcStatus.color);
 };
 
 btn.addEventListener("click", () => {
-  const peso = document.querySelector(".peso");
-  const altura = document.querySelector(".altura");
+  if (peso.value === "" || altura.value === "") {
+    alert("Nenhum campo pode estár vazio");
+    return;
+  }
+
   const myIMC = Number(peso.value) / (Number(altura.value) * 2);
   peso.value = "";
   altura.value = "";
