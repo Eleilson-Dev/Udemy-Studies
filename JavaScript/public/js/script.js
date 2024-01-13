cbecb703eb2bccfe138e040ef1ad3e5e904ebe52
Eleilson-Dev <maxta.gamer@gmail.com>
2024-01-12 22:34:05 -0300

    update

--- JavaScript/public/js/script.js
+++ JavaScript/public/js/script.js
@@ -1,14 +1,14 @@
 const users = [
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
-  { key: 1, name: "Eleilson", lastName: "sobrenome", level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
+  { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
 ];
 
 console.table(users);
