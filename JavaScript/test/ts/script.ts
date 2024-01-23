f3a95d3af19366d54797b1f02e85fbe33adc2e2e
Eleilson-Dev <maxta.gamer@gmail.com>
2024-01-23 20:38:47 -0300

    update

--- JavaScript/test/ts/script.ts
+++ JavaScript/test/ts/script.ts
@@ -1,4 +1,11 @@
-const users = [
+interface ArrOBJ {
+  key: number;
+  name: string;
+  lastName: string;
+  level: number;
+}
+
+const users: ArrOBJ[] = [
   { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
   { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
   { key: 1, name: 'Eleilson', lastName: 'sobrenome', level: 69 },
